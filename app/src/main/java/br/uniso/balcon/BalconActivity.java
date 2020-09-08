521201848e2ddccdb74a70f488ff592720cac685
Luís Morais <67266569+lmsmorais@users.noreply.github.com>
2020-09-08 10:23:08 -0300

    initializing the application, inicializando a aplicação

---
+++ app/src/main/java/br/uniso/balcon/BalconActivity.java
@@ -0,0 +1,14 @@
+package br.uniso.balcon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BalconActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
